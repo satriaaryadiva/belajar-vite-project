89ec7c850c047c5d5ceb369af695d80ef84c366c
satria arya diva <satriaarya125@gmail.com>
2024-04-12 02:43:50 +0700

    state management

--- Redux.js
+++ Redux.js
@@ -1,26 +1,43 @@
-import { createStore } from "redux"
-// reducer
-
-const  cartReducer=(
-  state = 
-  {cart : [],}, 
-action ) => {
-  switch (action.type) {
-      case 'ADD_TO_CART':
-        return {
-            ...state,cart : [...state.cart, action.payload],
-        }; default:
-         return state
-  }
-}
-//store 
-const  store = createStore(cartReducer)
-console.log('oncreate store :', store.getState())
-
-
-
-// subscribe
+import { legacy_createStore } from "redux";
+// reducer 
+const  cartReducer =(
+  state = {
+    cart: [{id:10, qty: 2}],
+  },
+  action  
+  )=>{
+    switch(action.type){
+        case 'ADD_TO_CART':{
+          return{
+        ...state,
+        cart :[...state.cart, action.payload],
+        };
+  
+      }    default: return state}
+    }
 
 
 
-//dispact
+///store
+const  store = legacy_createStore(cartReducer);
+console.log(store.getState());
+
+
+
+
+//subcribe
+store.subscribe(() => {
+  console.log('store changed', store.getState());
+})
+
+
+//dispatch
+const  action1= {
+  type: 'ADD_TO_CART',
+  payload: {
+    id:3,
+    qty: 2
+  }}
+
+
+  store.dispatch(action1)
